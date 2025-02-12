f5cb0bfe15c449a68df03959c79a5fbeb6e472c1
Shreekant-3398 <shreekant@betalectic.com>
2025-02-12 11:03:23 +0530

    added web-view

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
 import { Image, Text, View } from "react-native";
+import {WebView} from 'react-native-webview';
+import Config from 'react-native-config';
 
 export default function Index() {
   return (
@@ -10,6 +12,11 @@ export default function Index() {
         backgroundColor: "#fff",
       }}
     >
+      <WebView
+        source={{
+          uri: `${Config.API_URL}`,
+        }}
+      />
       <Image
         source={require('../assets/images/BetalecticLogo.png')}
         style={{ width: 500, height: 100 }} // Adjust the width and height as needed
